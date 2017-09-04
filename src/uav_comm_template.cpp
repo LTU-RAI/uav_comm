5e0994ac9592673159d91d1c16415ac03c69bab4
Elias Small <eliasmoose@gmail.com>
2017-09-04 16:15:47 +0200

    Publishers done, subscriber started but non functional

--- src/uav_comm_template.cpp
+++ src/uav_comm_template.cpp
@@ -4,6 +4,13 @@
 #include "std_msgs/String.h"
 #include <sstream>
 #include "sensor_msgs/Imu.h"
+#include "mav_msgs/Status.h"
+#include "ros/transport_hints.h"
+#include "ros/common.h"
+#include "ros/forwards.h"
+#include <boost/lexical_cast.hpp>
+#include <ros/subscribe_options.h>
+
 using namespace std;
 
 sensor_msgs::Imu empty_imu()
@@ -19,20 +26,16 @@ sensor_msgs::Imu empty_imu()
   for(int i=0; i<9;++i)
     msg.orientation_covariance[i]=0;
 
-
   msg.angular_velocity.x = 0;
   msg.angular_velocity.y = 0;
   msg.angular_velocity.z = 0;
 
-
   for(int i=0; i<9;++i)
     msg.angular_velocity_covariance[i]=0;
-
 
   msg.linear_acceleration.x = 0;
   msg.linear_acceleration.y = 0;
   msg.linear_acceleration.z = 0;
-
 
   for(int i=0; i<9;++i)
     msg.linear_acceleration_covariance[i]=0;
@@ -40,21 +43,31 @@ sensor_msgs::Imu empty_imu()
   return msg;
 }
 
+void callback(mav_msgs::RollPitchYawrateThrust.msg)
+{
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "uav_comm_node");
   ros::NodeHandle n;
+
+  ros::Subscriber RPYrT_sub = n.subscribe("/RPYrT",10,callback(), ros::TransportHints().tcpNoDelay());
+  ros::Publisher status_pub = n.advertise<mav_msgs::Status>("/status", 5);
   ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("/imu", 5);
+
   ros::Rate loop (10);
   int count = 0;
   while (ros::ok())
   {
     sensor_msgs::Imu imu_msg;
+    mav_msgs::Status status_msg;
     imu_msg = empty_imu();
+    status_pub.publish(status_msg);
     imu_pub.publish(imu_msg);
     ros::spinOnce;
     loop.sleep();
   }
-  ROS_INFO ("test");
+
   return 0;
 }
